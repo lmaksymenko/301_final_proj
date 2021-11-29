353902becaf01d881aeef9baf6f1d2028725f533
lmaksymenko <leo.maksymenko@gmail.com>
2021-11-28 23:15:07 -0500

    eden change a thing

--- final_proj.R
+++ final_proj.R
@@ -1,42 +1,27 @@
 #set's working directory to current folder
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-getwd()#goal: use the previous 5 days of VaR of each sector to predict the next 
+#goal: use the previous 5 days of VaR of each sector to predict the next 
 #5 day VaR of the market
-
 
 library(PerformanceAnalytics)
 library(quantmod)
 
-
-getSymbols("SPY", from = "2007-01-01", to = "2010-12-31")
-spyret = dailyReturn(SPY$SPY.Adjusted, type = "log")[-1]
-
-
-head(spyret)
-
-
-vec = c(6, rep(5, 200))
-spl = split(spyret,rep(1:201,vec))
-spy_var = spy_var1[-1,]
-
-
 train = read.csv("train_data.csv", row.names = "X")
 tail(train)
-
 
 vec = c(6, rep(5, 200))
 spl = split(train,rep(1:201,vec))
 splvar = lapply(spl, VaR)
 sec_var = do.call(rbind.data.frame, splvar)
+spy_var = sec_var[-1,1]
+sec_var = sec_var[,-1] #gets rid of SPY
 sec_var1 = as.data.frame(apply(sec_var, 2, Lag, k=1))[-1,] # capital Lag lags the data forward
 head(sec_var1)
 data = data.frame(spy_var, sec_var1)
 
-#Data Exploration
+########################################Data Exploration and Plotting
 library(ggplot2)
 library(reshape2)
-
-
 data['index'] = 1:200
 df <- melt(data, id.vars  = 'index', variable.name = 'series')
 
@@ -44,6 +29,7 @@ df <- melt(data, id.vars  = 'index', variable.name = 'series')
 ggplot(df, aes(index, value)) +
   geom_line(aes(colour = series)) + 
   ggtitle("Value at Risk Over Time")
+
 
 
 #training data plot 
@@ -59,15 +45,14 @@ ggplot(df, aes(index, value)) +
 
 as.POSIXct(train$index)
 
-data = na.exclude(data)
 
 
+##############################################Linear Model
 
+data  = na.exclude(data)
 
 trainsub  = sample(nrow(data),0.8*nrow(data),replace=FALSE)
 
-
-#GENERAL LINEAR MODEL
 linmod = glm(spy_var~., data=data, subset = trainsub)
 summary(linmod)
 
@@ -79,11 +64,11 @@ ar2.mse = mean((pred-data$spy_var[-trainsub])^2)
 ar2.mse
 
 
+################################### Random Forest
 library(randomForest)
 
-#RANDOM FOREST
-rf.reg = randomForest(spy_var~VGT,data=data, subset = trainsub, 
-                      ntree=100,mtry=1,importance=TRUE, na.action = na.roughfix) #mtry is number of variables 
+rf.reg = randomForest(spy_var~.,data=data, subset = trainsub, 
+                      ntree=100,mtry=3,importance=TRUE, na.action = na.roughfix) #mtry is number of variables 
 rf.reg #Gives both the number of variables at each split but also provides out-of-bag estimate of error rate
 
 rf.train.mse = mean((data$spy_var[trainsub] - predict(rf.reg, data[trainsub,]))^2) #TRAIN MSE
@@ -92,3 +77,23 @@ rf.train.mse
 spyvar.rf.pred = predict(rf.reg, data[-trainsub,]) # Predict with bagging
 rf.MSE = mean((data$spy_var[-trainsub] - spyvar.rf.pred)^2)
 rf.MSE #Test MSE
+
+rf.reg$importance # MeanDecreaseGini is MDI
+# MeanDecreaseAccuracy is MDA (based on bagging errors)
+varImpPlot(rf.reg)
+
+#################################### PCA (data exploration)
+prdata = data[-1] #without direction
+pr.out=prcomp(prdata,scale=TRUE) #Make sure you scale the vectors for principal component analysis
+
+pr.var=pr.out$sdev^2 #Variance explained
+
+pve=pr.var/sum(pr.var) #Proportion of variance explained
+pve
+
+plot(cumsum(pve),xlab="Principal Component",ylab="Cumulative Proportion",ylim=c(0,1),type='b', main = "Proportion of Variance Explained")
+abline(h=0.8, col = "red")
+
+pr.out$rotation
+biplot(pr.out,scale=0)
+
