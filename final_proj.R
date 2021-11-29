684bd9ddf672fe7554abe4484611f2374d62a268
lmaksymenko <leo.maksymenko@gmail.com>
2021-11-29 00:11:29 -0500

    formating is yes

--- final_proj.R
+++ final_proj.R
@@ -3,26 +3,38 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 #goal: use the previous 5 days of VaR of each sector to predict the next 
 #5 day VaR of the market
 
+
+###IMPORTS
 library(PerformanceAnalytics)
 library(quantmod)
+library(randomForest)
+library(ggplot2)
+library(reshape2)
+
+
+### ### SETUP ### ###
 
 train = read.csv("train_data.csv", row.names = "X")
-tail(train)
+
+#tail(train)
 
 vec = c(6, rep(5, 200))
-spl = split(train,rep(1:201,vec))
+spl = split(train, rep(1:201,vec))
 splvar = lapply(spl, VaR)
 sec_var = do.call(rbind.data.frame, splvar)
 spy_var = sec_var[-1,1]
 sec_var = sec_var[,-1] #gets rid of SPY
 sec_var1 = as.data.frame(apply(sec_var, 2, Lag, k=1))[-1,] # capital Lag lags the data forward
-head(sec_var1)
-data = data.frame(spy_var, sec_var1)
 
-########################################Data Exploration and Plotting
-library(ggplot2)
-library(reshape2)
-data['index'] = 1:200
+#head(sec_var1)
+
+data = data.frame(spy_var, sec_var1)
+rownames(data) = 1:200
+
+
+### ### EDA ### ###
+
+
 df <- melt(data, id.vars  = 'index', variable.name = 'series')
 
 #create line plot for each column in data frame
@@ -45,27 +57,50 @@ ggplot(df, aes(index, value)) +
 
 as.POSIXct(train$index)
 
+###PCA (data exploration)
+prdata = data[-1] #without direction
+pr.out=prcomp(prdata,scale=TRUE) #Make sure you scale the vectors for principal component analysis
+
+pr.var=pr.out$sdev^2 #Variance explained
+
+pve=pr.var/sum(pr.var) #Proportion of variance explained
+pve
+
+plot(cumsum(pve),xlab="Principal Component",ylab="Cumulative Proportion",ylim=c(0,1),type='b', main = "Proportion of Variance Explained")
+abline(h=0.8, col = "red")
+
+pr.out$rotation
+biplot(pr.out,scale=0)
 
 
-##############################################Linear Model
+### ### MODELING ### ###
 
+###TRAIN TEST SPLIT
 data  = na.exclude(data)
-
 trainsub  = sample(nrow(data),0.8*nrow(data),replace=FALSE)
 
-linmod = glm(spy_var~., data=data, subset = trainsub)
-summary(linmod)
 
-linmod.train.mse = mean((data$spy_var[trainsub] - predict(linmod, data[trainsub,]))^2) #train MSE
-linmod.train.mse
+###LINEAR MODEL
+
+linmod = glm(spy_var~., data=data, subset = trainsub)
+
+linmod.train.mse = mean((data$spy_var[trainsub] - predict(linmod, data[trainsub,]))^2)
 
 pred = predict(linmod, data[-trainsub,])
-ar2.mse = mean((pred-data$spy_var[-trainsub])^2)
-ar2.mse
+linmod.test.mse = mean((pred - data$spy_var[-trainsub])^2)
 
 
-################################### Random Forest
-library(randomForest)
+cat('LINEAR MODEL \n',
+    'Summary: \n', 
+    strrep('-', 80),
+    sapply(capture.output(summary(linmod)), function(x) paste(x, '\n')), '\n',
+    strrep('-', 80), '\n', 
+    'Train MSE: ', linmod.train.mse, '\n',
+    'Test MSE: ', linmod.test.mse, '\n')
+
+
+
+###RANDOM FOREST
 
 rf.reg = randomForest(spy_var~.,data=data, subset = trainsub, 
                       ntree=100,mtry=3,importance=TRUE, na.action = na.roughfix) #mtry is number of variables 
@@ -82,18 +117,5 @@ rf.reg$importance # MeanDecreaseGini is MDI
 # MeanDecreaseAccuracy is MDA (based on bagging errors)
 varImpPlot(rf.reg)
 
-#################################### PCA (data exploration)
-prdata = data[-1] #without direction
-pr.out=prcomp(prdata,scale=TRUE) #Make sure you scale the vectors for principal component analysis
 
-pr.var=pr.out$sdev^2 #Variance explained
-
-pve=pr.var/sum(pr.var) #Proportion of variance explained
-pve
-
-plot(cumsum(pve),xlab="Principal Component",ylab="Cumulative Proportion",ylim=c(0,1),type='b', main = "Proportion of Variance Explained")
-abline(h=0.8, col = "red")
-
-pr.out$rotation
-biplot(pr.out,scale=0)
 
