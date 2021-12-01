a9d32f83ec2e5c8641e84fdfd5503e89ac24fbb1
lmaksymenko <leo.maksymenko@gmail.com>
2021-12-01 17:00:43 -0500

    fix nn, add drop na

--- final_proj.R
+++ final_proj.R
@@ -35,21 +35,22 @@ sec_var = do.call(rbind.data.frame, splvar)
 spy_var_test = sec_var[-1,1]
 sec_var = sec_var[,-1] #gets rid of SPY
 sec_var1_test = as.data.frame(apply(sec_var, 2, Lag, k=1))[-1,]
+
 #head(sec_var1)
 
 data_train = data.frame(spy_var_train, sec_var1_train)
-data_train['index'] = 1:200
-
 data_test = data.frame(spy_var_test, sec_var1_test)
 
+data_train = na.omit(data_train)
+data_test = na.omit(data_test)
+
+data_train['index'] = 1:nrow(data_train)
+data_test['index'] = 1:nrow(data_test)
 
 head(data_test)
 
 
 ### ### EDA ### ###
-
-data_test['index'] = 1:144
-data_train['index'] = 1:200
 
 df <- melt(data, id.vars  = 'index', variable.name = 'series')
 
@@ -109,6 +110,8 @@ pred = predict(linmod, data_test)
 linmod.test.mse = mean((pred - data_test$spy_var_test)^2)
 
 
+#Results
+
 cat('LINEAR MODEL \n',
     'Summary: \n', 
     strrep('-', 80),
@@ -116,6 +119,8 @@ cat('LINEAR MODEL \n',
     strrep('-', 80), '\n', 
     'Train MSE: ', linmod.train.mse, '\n',
     'Test MSE: ', linmod.test.mse, '\n')
+
+
 
 plot(data_test$spy_var_test, type = "l", main = "LINEAR MODEL VaR Predicted vs Empirical",
      ylab = "VaR")
@@ -133,6 +138,7 @@ rf.train.mse = mean((data_train$spy_var_train - predict(rf.reg, data_train))^2)
 spyvar.rf.pred = predict(rf.reg, data_test) # Predict with bagging
 rf.test.mes = mean((data_test$spy_var_test - spyvar.rf.pred)^2)
 
+#Results
 
 cat('RANDOM FOREST MODEL \n',
     'Summary: \n', 
@@ -157,52 +163,72 @@ plot(data_test$spy_var_test, type = "l", main = "RANDOM FOREST VaR Predicted vs 
 lines(spyvar.rf.pred, lty = 2, col = "blue")
 legend(80, y = -0.05, c("Actual", "Predicted"), lty = c(1,2), col = c("black", "blue"))
 
-###NEURON ACTIVATION NETWORK
+
+
+###NEURON ACTIVATION NETWORKS
 #https://www.youtube.com/watch?reload=9&v=roUIWGr9rqo
 
-#Params
-HIDDEN_SIZE <- 128
-BATCH_SIZE <- 128
-LAYERS <- 2
+do_nn <- function()
 
-x_train = as.matrix(subset(data_train, select = -c(spy_var_train)))
-y_train = as.matrix(data_train[, "spy_var_train"])
+
+
+#Params
+HIDDEN_SIZE <- 20
+BATCH_SIZE <- 20
+LAYERS <- 5
+
+x_train = as.matrix(subset(data_train, select = -c(spy_var_train, index)))#, byrow = TRUE)
+#y_train = as.matrix(data_train[, "spy_var_train"])
+y_train = as.matrix(subset(data_train, select = c(spy_var_train)))
+
+x_train[4]
+
+nrow(y_train)
 
 #Build Model
 model <- keras_model_sequential() 
 
 model %>%
   #layer_lstm(HIDDEN_SIZE, input_shape=c(MAXLEN, length(char_table))) %>%
-  layer_dense(HIDDEN_SIZE, activation = 'relu', input_shape = c(11))
+  layer_dense(HIDDEN_SIZE, activation = 'linear', input_shape = ncol(x_train))
   
   for(i in 1:LAYERS){
-    model %>% layer_dense(HIDDEN_SIZE, activation = 'relu')
+    model %>% layer_dense(HIDDEN_SIZE, activation = 'linear')
   }
     
-  model %>% layer_dense(1, activation = 'relu')
-  
+  model %>% layer_dense(1, activation = 'linear')
   
   
 model %>% compile(
-  loss = "categorical_crossentropy", 
+  loss = "mean_squared_error", 
   optimizer = "adam", 
   metrics = "accuracy"
 )
+
 ###
 
 model %>% fit( 
   x = x_train, 
   y = y_train, 
   batch_size = BATCH_SIZE, 
-  epochs = 70
+  epochs = 15
 )
 
-x_test = as.matrix(subset(data_test, select = -c(spy_var_test)))
+x_test = as.matrix(subset(data_test, select = -c(spy_var_test, index)))
 
 result <- predict(model, x_test)
 
+#data_train['spy_var_train']
+nn.train.mse = mean((data_train[,'spy_var_train'] - predict(model, x_train)) ^ 2) 
+nn.train.mse
 
-plot(data_test$spy_var_test, type = "l", main = "RANDOM FOREST VaR Predicted vs Empirical",
+data_test['spy_var_test']
+nn.test.mse = mean((data_test[,'spy_var_test'] - result) ^ 2) 
+nn.test.mse
+
+#Results
+
+plot(data_test$spy_var_test, type = "l", main = "NN VaR Predicted vs Empirical",
      ylab = "VaR")
 lines(result, lty = 2, col = "blue")
 legend(80, y = -0.05, c("Actual", "Predicted"), lty = c(1,2), col = c("black", "blue"))
